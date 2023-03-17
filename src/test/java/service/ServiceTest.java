dd7ca347d509d52113f94398e4d7ac291f0558eb
916-Preda-Andrei <72196520+916-Preda-Andrei@users.noreply.github.com>
2023-03-17 17:35:40 +0200

    Initial commit

---
+++ src/test/java/service/ServiceTest.java
@@ -0,0 +1,52 @@
+package service;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceTest {
+
+    static Service service;
+
+    @BeforeAll
+    static void setUp(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "src/main/resources/repository/studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "src/main/resources/repository/teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "src/main/resources/repository/note.xml");
+
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    void testAddStudent(){
+        service.saveStudent("5", "Andrei", 936);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 4);
+        service.deleteStudent("5");
+    }
+
+    @Test
+    void testAddStudentExistingId(){
+        service.saveStudent("1", "Andrei", 936);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 3);
+    }
+}
