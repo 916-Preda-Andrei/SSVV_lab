c235a2556804b81529bd3484b2c2ed1ab1fd47c9
Andrei Popa <andrei.popa@analog.com>
2023-03-31 00:20:06 +0300

    added more tests for addStudent

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class ServiceTest {
 
-    private Service setUp(){
+    private Service setUp() {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
@@ -30,10 +30,11 @@ public class ServiceTest {
         return new Service(fileRepository1, fileRepository2, fileRepository3);
     }
 
+
     @Test
-    public void testAddStudent(){
+    public void testAddStudent() {
         Service service = setUp();
-        service.saveStudent("5", "Andrei", 936);
+        service.saveStudent("5", "Name", 936);
         List<Student> studenti = new ArrayList<>();
         service.findAllStudents().forEach(studenti::add);
         assert (studenti.size() == 4);
@@ -41,9 +42,57 @@ public class ServiceTest {
     }
 
     @Test
-    public void testAddStudentExistingId(){
+    public void testAddStudentExistingId() {
         Service service = setUp();
-        service.saveStudent("1", "Andrei", 936);
+        service.saveStudent("1", "Name", 936);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 3);
+    }
+
+    @Test
+    public void testAddStudentExistingName() {
+        Service service = setUp();
+        service.saveStudent("3", "Maria", 222);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 4);
+        service.deleteStudent("3");
+    }
+
+    @Test
+    public void testAddStudentNonNumericId() {
+        Service service = setUp();
+        service.saveStudent("abc", "Name", 936);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 4);
+        service.deleteStudent("abc");
+    }
+
+    @Test
+    public void testAddStudentNegativeId() {
+        Service service = setUp();
+        service.saveStudent("-1", "Name", 936);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 4);
+        service.deleteStudent("-1");
+    }
+
+    @Test
+    public void testAddStudentInvalidNegativeGroup() {
+        Service service = setUp();
+        service.saveStudent("5", "Name", -1);
+        List<Student> studenti = new ArrayList<>();
+        service.findAllStudents().forEach(studenti::add);
+        assert (studenti.size() == 3);
+    }
+
+    @Test
+    public void testAddStudentInvalidInvalidGroup() {
+        Service service = setUp();
+        service.saveStudent("5", "Name", 1);
         List<Student> studenti = new ArrayList<>();
         service.findAllStudents().forEach(studenti::add);
         assert (studenti.size() == 3);
