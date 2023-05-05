7b0cc1a14183f69fc8d29ed388282bbf049a1584
Andrei Popa <andrei.popa@analog.com>
2023-05-05 12:34:41 +0300

    added all test cases for AddAssignment

--- src/test/java/repository/TemaXMLRepositoryTest.java
+++ src/test/java/repository/TemaXMLRepositoryTest.java
@@ -26,14 +26,76 @@ public class TemaXMLRepositoryTest {
         temaXMLRepository.delete("4");
     }
 
-    @Test
+    @Test //(tema.getDeadline() < 1 || tema.getDeadline() > 14 || tema.getDeadline() < tema.getStartline())
     public void testAddAssignmentInvalidDeadline() {
         TemaXMLRepository temaXMLRepository = setUp();
-        Tema tema = new Tema("4", "tema test", 15, 8);
-        temaXMLRepository.save(tema);
+        int size = temaXMLRepository.entities.size();
 
-        Tema temaSalvata = temaXMLRepository.entities.get("4");
-        assert (temaSalvata == null);
-        assert (temaXMLRepository.entities.size() == 1);
+        Tema tema1 = new Tema("4", "tema test", 0, 8);
+        temaXMLRepository.save(tema1);
+        assert (temaXMLRepository.entities.size() == size);
+
+        Tema tema2 = new Tema("4", "tema test", 15, 8);
+        temaXMLRepository.save(tema2);
+        assert (temaXMLRepository.entities.size() == size);
+
+        Tema tema3 = new Tema("4", "tema test", 10, 11);
+        temaXMLRepository.save(tema3);
+        assert (temaXMLRepository.entities.size() == size);
+    }
+
+    @Test
+    public void testAddAssignmentInvalidID() {
+        TemaXMLRepository temaXMLRepository = setUp();
+        int size = temaXMLRepository.entities.size();
+
+        Tema tema1 = new Tema("", "tema test", 10, 8);
+        temaXMLRepository.save(tema1);
+        assert(size == temaXMLRepository.entities.size());
+
+        Tema tema2 = new Tema(null, "tema test", 10, 8);
+        temaXMLRepository.save(tema2);
+        assert(size == temaXMLRepository.entities.size());
+    }
+
+    @Test
+    public void testAddAssignmentInvalidDesc() {
+        TemaXMLRepository temaXMLRepository = setUp();
+        int size = temaXMLRepository.entities.size();
+
+        Tema tema1 = new Tema("4", "", 10, 8);
+        temaXMLRepository.save(tema1);
+        assert(size == temaXMLRepository.entities.size());
+
+        Tema tema2 = new Tema("4", null, 10, 8);
+        temaXMLRepository.save(tema2);
+        assert(size == temaXMLRepository.entities.size());
+    }
+
+    @Test
+    public void testAddAssignmentInvalidStartLine() {
+        TemaXMLRepository temaXMLRepository = setUp();
+        int size = temaXMLRepository.entities.size();
+
+        Tema tema1 = new Tema("5", "Desc", 10, 0);
+        temaXMLRepository.save(tema1);
+        assert(size == temaXMLRepository.entities.size());
+
+        Tema tema2 = new Tema("5", "Desc", 10, 15);
+        temaXMLRepository.save(tema2);
+        assert(size == temaXMLRepository.entities.size());
+
+        Tema tema3 = new Tema("5", "Desc", 10, 11);
+        temaXMLRepository.save(tema3);
+        assert(size == temaXMLRepository.entities.size());
+    }
+
+    @Test
+    public void testAddAssignmentAlreadyAdded() {
+        TemaXMLRepository temaXMLRepository = setUp();
+        Tema tema = new Tema("4", "tema test", 10, 8);
+        assert (temaXMLRepository.save(tema) == null);
+        assert (temaXMLRepository.save(tema) != null);
+        temaXMLRepository.delete("4");
     }
 }
